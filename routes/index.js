5dec081e21683e6169ceb6601eb101cb0e291130
Soufii <abbshr@outlook.com>
2014-04-11 21:41:08 +0800

    add router '/' and '*'

--- routes/index.js
+++ routes/index.js
@@ -5,21 +5,22 @@
 
 module.exports = function (app) {
   app.get('/', function (req, res) {
-    if (req.session) res.render('index', { user: req.session.user });
-    else res.render('index', { user: null });
+    var session_user = req.session && req.session.user;
+    if (session_user) res.render('index', { user: session_user, title: session_user.name + '-incbrain' });
+    else res.render('index', { user: null, title: 'incbrain, fork your ideas' });
   });
-  app.get('/explorer', function (req, res) {
+  /*app.get('/explorer', function (req, res) {
     if (req.session)
     // get hot ideas from storage
     else res.redirect('/'); 
   });
-  app.get('/signup');
-  app.get('/login');
-  app.get('/idea');
-  app.post('/');
-  app.post('/signup');
-  app.post('/login');
-  app.all('*', function () {
-    res.render('404');
+  //app.get('/signup');
+  //app.get('/login');
+  //app.get('/idea');
+  //app.post('/');
+  //app.post('/signup');
+  //app.post('/login');*/
+  app.all('*', function (req, res) {
+    res.render('404', { content: 'r u serious?' });
   });
 };
